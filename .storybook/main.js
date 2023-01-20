ca0a8ceeebf9a700d8bfb3ba1c0182c3188cfb8b
adolfogustavo <gustavodominguez230@gmail.com>
2023-01-20 17:18:23 -0300

    feat: config ts and adding drag and drop list

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.tsx"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
@@ -13,5 +13,14 @@ module.exports = {
   },
   features: {
     interactionsDebugger: true,
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
   },
 };
